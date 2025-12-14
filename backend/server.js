e3eb360f03b62fd1243c837776e642428c08ad18
Nomhle Madubandlela <parencia54@gmail.com>
2025-12-15 01:09:03 +0200

    Fix render deploy

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-
-
 const authRoutes = require('./routes/auth');
 const protectedRoutes = require('./routes/protected');
 const userRoutes = require('./routes/users');
@@ -17,12 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('*', (req, res, next) => {
-    console.log('Request URL:', req.originalUrl);
-    next();
-});
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const MONGO = process.env.MONGO_URI;
 console.log("Mongo URI:", process.env.MONGO_URI);
 
